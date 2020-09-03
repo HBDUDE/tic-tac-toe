2152cb3a5c6e80a755c5a8e260c2622e35fb831f
HBDUDE <supersuniiside@gmail.com>
2020-09-03 09:40:03 -0600

    Tic-tac-toe angular tutorial

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <button>{{ value }}</button>
+  `,
+  styles: [`
+    button {
+      width: 100%;
+      height: 100%;
+      font-size: 2rem;
+    }
+  `]
+})
+export class SquareComponent {
+
+  constructor() { }
+  @Input() value: 'X' | 'O';
+
+}
